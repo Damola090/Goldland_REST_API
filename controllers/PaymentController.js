21d932923d815c3d679a485b27b2c599ff9d574c
Damola090 <tundebalo090@gmail.com>
2023-07-30 17:49:44 +0100

    update_paystack

--- controllers/PaymentController.js
+++ controllers/PaymentController.js
@@ -6,7 +6,6 @@ const SendPaystackKey = async (req, res, next) => {
 
     res.status(200).json({
         publicKey : private_Key
-        // publicKey : 'pk_live_4e5cb1d250c4e25de2e20dbff951ecc71152fd15'
     })            
 
 }
